c7173e54f41476e8813863660c76c8577c224f54
Janzel25 <j.claudio@cfacampusmontsouris.fr>
2026-04-01 16:00:53 +0200

    Update student name in info route

--- app/app.py
+++ app/app.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 def info():
     return {
         "app": "Flask Render",
-        "student": "VOTRE_NOM",
+        "student": "CLAUDIO Janzel",
         "version": "v1"
     }
 
